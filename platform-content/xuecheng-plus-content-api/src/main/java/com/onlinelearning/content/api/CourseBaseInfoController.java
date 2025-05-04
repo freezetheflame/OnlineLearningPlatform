507245b76041efca58cfd39d505960b8a607af90
hectum <108981342+freezetheflame@users.noreply.github.com>
2025-05-04 18:21:58 +0800

    system service & CORS

--- platform-content/xuecheng-plus-content-api/src/main/java/com/onlinelearning/content/api/CourseBaseInfoController.java
+++ platform-content/xuecheng-plus-content-api/src/main/java/com/onlinelearning/content/api/CourseBaseInfoController.java
@@ -1,11 +1,13 @@
 package com.onlinelearning.content.api;
 
+import com.onlinelearning.content.service.CourseBaseInfoService;
 import com.onlinelearning.platformbase.model.PageParams;
 import com.onlinelearning.platformbase.model.PageResult;
 import com.onlinelearning.content.model.dto.QueryCourseParamsDto;
 import com.onlinelearning.content.model.po.CourseBase;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CourseBaseInfoController {
 
+    @Autowired
+    CourseBaseInfoService courseBaseInfoService;
+
     @ApiOperation("课程查询接口")
     @PostMapping("/course/list")
     public PageResult<CourseBase> list(PageParams pageParams, @RequestBody(required = false) QueryCourseParamsDto queryCourseParams){
-
-        return null;
-
+        return courseBaseInfoService.queryCourseBaseList(pageParams, queryCourseParams);
     }
 
 }
--- platform-content/xuecheng-plus-content-service/src/main/java/com/onlinelearning/content/service/impl/CourseBaseInfoServiceImpl.java
+++ platform-content/xuecheng-plus-content-service/src/main/java/com/onlinelearning/content/service/impl/CourseBaseInfoServiceImpl.java
@@ -36,7 +36,7 @@ public class CourseBaseInfoServiceImpl implements CourseBaseInfoService {
         queryWrapper.eq(StringUtils.isNotEmpty(queryCourseParamsDto.getAuditStatus()), CourseBase::getAuditStatus, queryCourseParamsDto.getAuditStatus());
         //构建查询条件，根据课程发布状态查询
         //todo:根据课程发布状态查询
-
+        queryWrapper.eq(StringUtils.isNotEmpty(queryCourseParamsDto.getPublishStatus()),CourseBase::getStatus,queryCourseParamsDto.getPublishStatus());
         //分页对象
         Page<CourseBase> page = new Page<>(pageParams.getPageNo(), pageParams.getPageSize());
         // 查询数据内容获得结果
